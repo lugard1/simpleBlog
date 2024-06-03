aa22a667b8d9605a6917fa787f18e25a9ea185c0
Lugard Agu <agulugard@gmail.com>
2024-06-03 15:26:22 +0100

    Implement methods and views to edit comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post) # Redirect to the post show page
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:username, :body)
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  validates :username, presence: true, length: {minimum: 5}
+  validates :body, presence: true
 end
