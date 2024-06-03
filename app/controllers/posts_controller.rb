d84d9d002db3152132a5d4b26bfac4b4adb30881
Lugard Agu <agulugard@gmail.com>
2024-06-03 13:01:46 +0100

    Generate model for comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,18 +3,6 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def new
     @post = Post.new
   end
@@ -28,6 +16,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end  
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path,  notice: 'Post was successfully deleted.'
+  end
+
   private
 
   def post_params
@@ -35,29 +46,3 @@ class PostsController < ApplicationController
   end
 
 end
-
-# class PostsController < ApplicationController
-#   def index
-#     @posts = Post.all
-#   end
-
-#   def new
-#     @post = Post.new
-#   end
-
-#   def create
-#     @post = Post.new(post_params)
-#     if @post.save
-#       redirect_to @post, notice: 'Post was successfully created.'
-#     else
-#       render :new, status: :unprocessable_entity
-#     end
-#   end
-
-#   private
-
-#   def post_params
-#     params.require(:post).permit(:title, :body)
-#   end
-# end
-
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// import "@hotwired/turbo-rails"
+// import "controllers"
+import Rails from '@rails/ujs';
+
+// Rails.start();
+
+// import Rails from "rails-ujs";\\='
+
+import Turbolinks from "turbolinks";
+import { Turbo } from "@hotwired/turbo-rails";
+
+Rails.start();
+Turbolinks.start();
+Turbo.start();
